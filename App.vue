73b0ed17c934069c9bc9522bdb6544f4edcc23d3
marcin7777 <marcinmz9@tlen.pl>
2020-04-25 14:31:21 +0200

    Login code added

--- App.vue
+++ App.vue
@@ -1,3 +1,24 @@
+ GNU nano 4.6                                    App.vue
+<template>
+  <div id="app">
+   <h1>Twój email to: {{email}}</h1>
+
+   <div v-if="email.length < 10">Ale masz krótki adres!</div>
+   <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+   <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+
+   <input type = "text" v-model="email">
+   <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+ data() {
+ return {
 <template>
   <div id="app">
    <h1>Twój email to: {{email}}</h1>
