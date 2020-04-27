32d5e2b1d803aaf5c67120a983c57f058a215ac9
marcin7777 <marcinmz9@tlen.pl>
2020-04-27 21:19:53 +0200

    E-mail alert added

--- App.vue
+++ App.vue
@@ -6,6 +6,15 @@
     <div v-if="email.length < 10">Ale masz krótki adres!</div>
     <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
     <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+<template>
+  <div id="app">
+    
+    <h1>Twój e-mail to: {{ email }}</h1>
+    <input type="email" v-model="email">
+    <div v-if="email.length < 10">Ale masz krótki adres!</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
   </div>
 </template>
 
@@ -19,6 +28,13 @@ export default {
 		    email: '',
 		    password: ''
 		  };
+		  
+		},
+	
+	methods: {
+		  alertMyEmail() {
+		   alert(this.email);
+		  }
 		}
 	
 }
@@ -29,3 +45,4 @@ export default {
 
 
 </style>
+
